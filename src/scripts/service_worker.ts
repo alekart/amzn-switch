e76958938621c5d9f005d72bf8c1c3c46e96428f
alek <alekart@users.noreply.github.com>
2023-07-02 21:06:51 +0200

    Detect price with a better element present in products page

--- src/scripts/service_worker.ts
+++ src/scripts/service_worker.ts
@@ -1,4 +1,4 @@
-import { parse } from 'node-html-parser';
+import { HTMLElement, parse } from 'node-html-parser';
 import { Price } from './interfaces';
 
 chrome.runtime.onConnect.addListener(function (port) {
@@ -23,13 +23,32 @@ function fetchPrice(url: string): Promise<Price | null> {
     return r.text();
   }).then((html) => {
     const tree = parse(html);
-    const priceElement = tree.querySelector('.priceToPay');
-    if (!priceElement) {
-      return null;
-    }
-    const whole = tree.querySelector('.a-price-whole')?.innerText.replace(/\D/g, '');
-    const decimals = tree.querySelector('.a-price-fraction')?.innerText.replace(/\D/g, '');
-    const symbol = tree.querySelector('.a-price-symbol')?.innerText.trim();
-    return {whole, decimals, symbol};
+    return findPriceToPay(tree) || findPrice(tree);
   });
+}
+
+function findPrice(tree: HTMLElement): Price | null {
+  const priceElement = tree.querySelector('.priceToPay');
+  if (!priceElement) {
+    return null;
+  }
+  const whole = tree.querySelector('.a-price-whole')?.innerText.replace(/\D/g, '');
+  const decimals = tree.querySelector('.a-price-fraction')?.innerText.replace(/\D/g, '');
+  const symbol = tree.querySelector('.a-price-symbol')?.innerText.trim();
+  return { whole, decimals, symbol };
+}
+
+function findPriceToPay(tree: HTMLElement): Price | null {
+  const priceElement = tree.querySelector('[data-feature-name=corePrice] .a-offscreen');
+  if (!priceElement) {
+    return null;
+  }
+  const isolateSymbol = (value: string): string => value.replace(/\d/g, '');
+  const isolateNumbers = (value: string): string => value.replace(/\D/g, '');
+  const text = priceElement.innerText;
+  const split = text.split(/[.,]/);
+  const symbol = isolateSymbol(split[0]) || isolateSymbol(split[1]);
+  const whole = isolateNumbers(split[0]);
+  const decimals = isolateNumbers(split[1]);
+  return { whole, decimals, symbol, text };
 }
